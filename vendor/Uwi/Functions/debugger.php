5e14e6a93b4ba06ae5332cb7763922877f22482d
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-07-05 00:23:56 +0300

    Add: dd function
    Dump and die

---
+++ vendor/Uwi/Functions/debugger.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Dump and die
+ *
+ * @param array $args
+ * @return void
+ */
+function dd(...$args): void
+{
+    echo '<pre>';
+    foreach ($args as $arg) {
+        var_dump($arg);
+        echo '<br><br>';
+    }
+    echo '</pre>';
+
+    exit;
+}
