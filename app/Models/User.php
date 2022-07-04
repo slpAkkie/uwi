05403a4d52cfaa055af6f46689927a68a7b89b97
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-07-05 00:24:12 +0300

    Move User.php model

---
+++ app/Models/User.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Uwi\Core\ORM\Model as Model;
+
+class User extends Model
+{
+    // TODO: Implement...
+}
--- vendor/Uwi/Http/Models/User.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Http\Models;
-
-class User
-{
-    // TODO: Implement
-}
