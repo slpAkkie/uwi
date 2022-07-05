56882af41b638947e17cce225457ab1124ae7f2e
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-07-05 10:58:41 +0300

    Update: Uwi\Http\Request Initialization

--- vendor/Uwi/Http/Request/Request.php
+++ vendor/Uwi/Http/Request/Request.php
@@ -5,10 +5,41 @@ namespace Uwi\Http\Request;
 class Request
 {
     /**
+     * User agent
+     *
+     * @var string
+     */
+    public readonly string $userAgent;
+
+    /**
+     * HTTP host
+     *
+     * @var string
+     */
+    public readonly string $httpHost;
+
+    /**
+     * Request method
+     *
+     * @var string
+     */
+    public readonly string $requestMethod;
+
+    /**
+     * Request URI
+     *
+     * @var string
+     */
+    public readonly string $requestURI;
+
+    /**
      * Initialize the Request
      */
     public function __construct()
     {
-        //
+        $this->userAgent = $_SERVER['HTTP_USER_AGENT'];
+        $this->httpHost = $_SERVER['HTTP_HOST'];
+        $this->requestMethod = $_SERVER['REQUEST_METHOD'];
+        $this->requestURI = $_SERVER['REQUEST_URI'];
     }
 }
