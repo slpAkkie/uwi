57cb676d87ba296e052157c19cb5264850d1b979
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-07-08 01:08:41 +0300

    Update: Uwi\Support\Collection

--- vendor/Uwi/Framework/Support/Collection.php
+++ vendor/Uwi/Framework/Support/Collection.php
@@ -2,7 +2,44 @@
 
 namespace Uwi\Support;
 
+use Closure;
+
 class Collection
 {
-    // TODO: Implement...
+    /**
+     * Collection items
+     *
+     * @var array
+     */
+    private array $items = [];
+
+    /**
+     * Make new Collection from value
+     *
+     * @param mixed $val
+     * @return static
+     */
+    public static function make(mixed $val): static
+    {
+        $instance = new static();
+
+        if (is_array($val)) {
+            $instance->items = $val;
+        } else {
+            $instance->items[] = $val;
+        }
+
+        return $instance;
+    }
+
+    /**
+     * Apply callback on each item in collection and return new collection
+     *
+     * @param Closure $callback
+     * @return static
+     */
+    public function map(Closure $callback): static
+    {
+        return static::make(array_map($callback, $this->items));
+    }
 }
