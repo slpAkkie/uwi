3a16b01b42b908f6c9fc44c2a0001c3971e75029
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-07-08 10:04:31 +0300

    Update: Lion

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Uwi\Foundation\Http\Controllers\Controller as BaseController;
 
 class Controller extends BaseController
 {
     public function welcome()
     {
+        dd(User::raw('select * from users where id = ?', [1]));
         return view('welcome');
     }
 }
