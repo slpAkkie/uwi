1e19e219839394c6be8f58b7017a2d1b71e09077
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2022-10-08 22:24:31 +0300

    Webpack: Remove Pug and change structure of src

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./resources/**/*.{pug,scss}'],
+  content: ['./views/**/*.clbr.html'],
 
   theme: {
     extend: {
--- webpack.config.js
+++ webpack.config.js
@@ -1,85 +1,47 @@
 const { glob } = require('glob')
 const path = require('path')
-const PugPlugin = require('pug-plugin')
-
-const assetPath = 'storage/app/assets/'
-
-function asset(_path) {
-    return path.join(assetPath, _path)
-}
 
 module.exports = {
-  // Set to production to minify JS files.
   mode: 'development',
 
-  // Handle all .pug files into the pages folder
-  // so all this pages will be an entry points.
-  entry: glob.sync('./resources/views/pages/*.pug').reduce((obj, el) => {
-    obj[path.parse(el).name] = el
+  entry: glob.sync('./src/ts/*.ts').reduce((entries, el) => {
+    entries[path.parse(el).name] = el
 
-    return obj
+    return entries
   }, {}),
 
-  // Set output options.
   output: {
-    path: __dirname,
+    path: path.join(__dirname, 'storage/app/dist/'),
     publicPath: '/',
-    filename: asset('js/[name].compiled.js'),
+    filename: '[name].bundle.js',
+    clean: true,
   },
 
-  plugins: [
-    // Enable processing of Pug files defined in webpack entry.
-    new PugPlugin({
-      extractCss: {
-        filename: asset('css/[name].compiled.css'),
-        },
-      filename: '/views/[name].clbr.html',
-    }),
-  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
 
-  // Needs to use require typescript files inside pug.
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
-    alias: {
-      '@': path.join(__dirname, 'resources/'),
-    },
   },
 
   module: {
     rules: [
       {
-        test: /\.(png|jpg|jpeg|svg|webp|ico)/,
-        type: 'asset/resource',
-        generator: {
-          filename: asset('img/[name][ext]'),
-        },
-      },
-      {
-        test: /\.(woff2|woff|ttf|otf|svg|eot)/,
-        type: 'asset/resource',
-        generator: {
-          filename: asset('fonts/[name][ext]'),
-        },
-      },
-      {
-        test: /\.(pug|html)$/,
-        loader: PugPlugin.loader,
-        options: {
-          method: 'render',
-          embedFilters: {
-            escape: true,
-            markdown: {
-                highlight: {
-                verbose: true,
-                use: 'prismjs',
-              },
-            },
-          },
-        },
-      },
-      {
         test: /\.(css|sass|scss)$/,
-        use: ['css-loader', 'postcss-loader', 'sass-loader'],
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          {
+            loader: "sass-loader",
+            options: {
+              sassOptions: { includePaths: ['./node_modules'] }
+            }
+          }
+        ],
       },
       {
         test: /\.tsx?$/,
