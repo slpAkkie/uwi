da3b709c5ebb04b95a6e840bee800e37dc168d6b
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2023-02-09 21:17:19 +0300

    Fix request: prevent multiple session and cookie instances

--- Framework/Services/Request.php
+++ Framework/Services/Request.php
@@ -2,10 +2,17 @@
 
 namespace Services;
 
+use Framework\Foundation\Contracts\ApplicationContract;
 use Services\Container\Contracts\SingletonContract;
+use Services\Http\Contracts\Cookies\CookieContract;
+use Services\Http\Contracts\Sessions\SessionContract;
 use Services\Http\Requests\Request as BaseRequest;
 
 class Request extends BaseRequest implements SingletonContract
 {
-    //
+    public function __construct(ApplicationContract $app)
+    {
+        $this->_session = $app->get(SessionContract::class);
+        $this->_cookie = $app->get(CookieContract::class);
+    }
 }
